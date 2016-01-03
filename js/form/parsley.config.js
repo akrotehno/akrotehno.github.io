bab48b9a52861860df6222fa769eaaa6febda173
Raphael Varonos <r.varonos@gmail.com>
2016-01-03 16:15:39 +0200

    Donation

--- js/form/parsley.config.js
+++ js/form/parsley.config.js
@@ -1,5 +1,1 @@
-// ParsleyConfig definition if not already set
-window.ParsleyConfig = window.ParsleyConfig || {};
-
-ParsleyConfig.excluded = 'input[type=button], input[type=submit], input[type=reset]';
-ParsleyConfig.inputs = 'input, textarea, select, input[type=hidden], :hidden';
+window.ParsleyConfig=window.ParsleyConfig||{},ParsleyConfig.excluded="input[type=button], input[type=submit], input[type=reset]",ParsleyConfig.inputs="input, textarea, select, input[type=hidden], :hidden";
